0d483a2cc2d93fd881408e30de5f62514fd0afd7
CroDeusRex <isluga00@fesb.hr>
2015-12-21 21:08:22 +0100

    Povezivanje

---
+++ Seminar_NWT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Seminar_NWT.MvcApplication" Language="C#" %>
---
+++ Seminar_NWT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Seminar_NWT.Startup))]
+namespace Seminar_NWT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
